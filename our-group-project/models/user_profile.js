23145a0c9f2030a483008203e64509eb89da302b
Veronica Rojas <ar_veronica@yahoo.com>
2023-06-09 12:05:14 +0100

    create tables: users, user_profile

---
+++ our-group-project/models/user_profile.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User_profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User_profile.init({
+    user_id: DataTypes.INTEGER,
+    bio: DataTypes.STRING,
+    reason_to_adopt: DataTypes.STRING,
+    reason_to_give: DataTypes.STRING,
+    extra_ifo: DataTypes.STRING,
+    occupation: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User_profile',
+  });
+  return User_profile;
+};
