3209a0ce71be4e4c25c7a96ceb3baf01a7dfd0e0
loretta savvidou <loretta.sav.6@gmail.com>
2023-06-09 14:40:08 +0200

    creating pets and breeds table models and migrations

---
+++ our-group-project/models/pets.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pet.init({
+    name: DataTypes.STRING,
+    breed_id: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER,
+    age: DataTypes.INTEGER,
+    gender: DataTypes.STRING,
+    neutered: DataTypes.BOOLEAN,
+    vaccination_status: DataTypes.BOOLEAN,
+    chip: DataTypes.BOOLEAN,
+    medical_issues: DataTypes.STRING,
+    special_needs: DataTypes.STRING,
+    passport: DataTypes.BOOLEAN,
+    bio: DataTypes.STRING,
+    diet: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pet',
+  });
+  return Pet;
+};
