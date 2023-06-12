80dfe42e2a823ee8bc0372ad634b3295e1a47b6a
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-06-12 15:57:33 +0200

    front-end Routes

--- our-group-project/client/src/App.jsx
+++ our-group-project/client/src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import Home from './assets/pages/Home';
+import About from './assets/pages/About';
+import UserProfile from './assets/pages/UserProfile';
+import ContactUs from './assets/pages/ContactUs';
 
 function App() {
-  return(
-  
-      <>
-      </>
-  )
+  return (
+    <>
+      <nav>
+        <Link to="/">Home </Link>
+        <Link to="/about">About </Link>
+        <Link to="/user_profile">User Profile </Link>
+        <Link to="/contact_us">Contact us</Link>
+      </nav>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user_profile" element={<UserProfile />} />
+          <Route path="/contact_us" element={<ContactUs />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
