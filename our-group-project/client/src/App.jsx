470bd7612a0f89f2b11ab8a195c24233f38513fe
loretta savvidou <loretta.sav.6@gmail.com>
2023-06-13 19:25:20 +0200

    authcontext and provider

--- our-group-project/client/src/App.jsx
+++ our-group-project/client/src/App.jsx
@@ -1,40 +1,57 @@
-import { useState } from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
-import Home from './assets/pages/Home';
-import About from './assets/pages/About';
-import UserProfile from './assets/pages/UserProfile';
-import ContactUs from './assets/pages/ContactUs';
-import Forum from './assets/pages/Forum';
-import PetProfile from './assets/components/PetProfile';
-import Navbar from './assets/components/Navbar';
-import Login from './assets/components/Login';
-import Quiz from './assets/components/Quiz';
-import Registration from './assets/components/Registration';
-import SearchPets from './assets/pages/SearchPets';
+import { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import AuthContext from "./assets/contexts/AuthContext";
+import Home from "./assets/pages/Home";
+import About from "./assets/pages/About";
+import UserProfile from "./assets/pages/UserProfile";
+import ContactUs from "./assets/pages/ContactUs";
+import Forum from "./assets/pages/Forum";
+import PetProfile from "./assets/components/PetProfile";
+import Navbar from "./assets/components/Navbar";
+import Login from "./assets/components/Login";
+import Quiz from "./assets/components/Quiz";
+import Registration from "./assets/components/Registration";
+import SearchPets from "./assets/pages/SearchPets";
 import "./App.css";
 
-
-
 function App() {
+  //preparing a global state + login + logout functions so that
+  //anybody within the whole app can call these functions
+  const [user, setUser] = useState(null);
+
+  function login(username, password) {
+    console.log("login");
+  }
+
+  function logout() {
+    console.log("logout");
+  }
+
+  const authObj = {
+    user,
+    login,
+    logout,
+  };
+
   return (
-    <>
+    <AuthContext.Provider value={authObj}>
       <Navbar />
       <div>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/user_profile" element={<UserProfile/>}/>
-          <Route path="/contact_us" element={<ContactUs/>}/>
-          <Route path="/pet_profile" element={<PetProfile/>} />
-          <Route path="/forum" element={<Forum/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/quiz" element={<Quiz/>}/>
-          <Route path="/registration" element={<Registration/>}/>
-          <Route path="search_pets" element={<SearchPets/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user_profile" element={<UserProfile />} />
+          <Route path="/contact_us" element={<ContactUs />} />
+          <Route path="/pet_profile" element={<PetProfile />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="search_pets" element={<SearchPets />} />
         </Routes>
       </div>
-    </>
+    </AuthContext.Provider>
   );
-};
+}
 
 export default App;
--- our-group-project/client/src/assets/components/Login.jsx
+++ our-group-project/client/src/assets/components/Login.jsx
@@ -10,13 +10,13 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import axios from 'axios';
+import axios from "axios";
+import { useState, useContext } from "react";
+import AuthContext from "../contexts/AuthContext";
 
 const api = axios.create({
   baseURL: "http://localhost:4000/api/auth",
 });
-
-
 
 function Copyright(props) {
   return (
@@ -40,84 +40,112 @@ function Copyright(props) {
 //const defaultTheme = createTheme();
 
 export default function Login() {
-  
+  const auth = useContext(AuthContext); //this will have user, login and logout functions
+
+  const [credentials, setCredentials] = useState({
+    username: "test",
+    password: "test",
+  });
+
+  const [data, setData] = useState(null);
+
+  const { username, password } = credentials;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const login = async () => {
+    try {
+      const { data } = await axios("/api/auth/login", {
+        method: "POST",
+        data: credentials,
+      });
+
+      localStorage.setItem("token", data.token);
+      auth.login();
+      console.log(data.message, data.token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logout = () => {
+    auth.logout();
+    localStorage.removeItem("token");
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    
-    };
+  };
 
   return (
-
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={login}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link to="/#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link to="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+            Login
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link to="/#" variant="body2">
+                Forgot password?
+              </Link>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link to="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }
