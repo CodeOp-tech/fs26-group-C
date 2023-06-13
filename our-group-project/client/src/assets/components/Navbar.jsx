c4d6851325ea0c93f7ba535494c07cc5e86deef9
loretta savvidou <loretta.sav.6@gmail.com>
2023-06-13 13:41:52 +0200

    navbar links and buttons

--- our-group-project/client/src/assets/components/Navbar.jsx
+++ our-group-project/client/src/assets/components/Navbar.jsx
@@ -1,17 +1,59 @@
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <nav>
-    <Link to="/">Home </Link>
-    <Link to="/about">About </Link>
-    <Link to="/user_profile">User Profile </Link>
-    <Link to="/forum">Forum </Link>
-    <Link to="/contact_us">Contact us </Link>
-    <Link to="/search_pets">Find a Pet</Link>
-    
-  </nav>
+    <div>
+      <nav>
+        {isLoggedIn ? (
+          <div>
+            <ul style={{listStyle: "none", display:"flex", justifyContent:"space-around"}}>
+              <li>
+                <Link to="/">Home </Link> <Link to="/about">About </Link>
+              </li>
+              <li>
+                <Link to="/user_profile">User Profile </Link>
+              </li>
+              <li>
+                <Link to="/forum">Forum </Link>
+              </li>
+              <li>
+                <Link to="/contact_us">Contact us </Link>
+              </li>
+              <li>
+                <Link to="/search_pets">Find a Pet</Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  <button> Logout</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div>
+            <ul style={{listStyle: "none", display:"flex", justifyContent:"space-around"}}>
+              <li>
+                <Link to="/">Home </Link> <Link to="/about">About </Link>
+              </li>
+              <li>
+                <Link to="/forum">Forum </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  <button> Login</button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/register">
+                  <button> Sign Up!</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
+    </div>
   );
 }
-
-
