2026d77f6808187cc92765f11e67796596c5d0c7
loretta savvidou <loretta.sav.6@gmail.com>
2023-06-13 13:21:46 +0200

    complete seeding for users + pets

--- our-group-project/seeders/20230613105611-demo-pets.js
+++ our-group-project/seeders/20230613105611-demo-pets.js
@@ -1,13 +1,10 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-
-    let pets = [];
-
-    const bot = 
-    {
+    await queryInterface.bulkInsert("pets", [
+      {
         name: "bot",
         breed_id: 5,
         user_id: 22,
@@ -18,12 +15,9 @@ module.exports = {
         chip: true,
         passport: true,
         bio: " i am not giving him away just advertising his AWESOMENESS",
-        personality: "best dog in the world.",
-        diet: "anything but his own kibble please."
-    }
-    
-    const fluff = 
-    {
+        diet: "anything but his own kibble please.",
+      },
+      {
         name: "fluff",
         breed_id: 28,
         user_id: 21,
@@ -34,49 +28,38 @@ module.exports = {
         chip: false,
         passport: true,
         bio: "not bot but..okay",
-        personality: "likes to eat and sleep",
-        diet: "only strawberries and champagne."
-    }
-  
-    
-    const sugarpuff = {
-      name: "sugarpuff",
-      breed_id: 12,
-      user_id: 25,
-      age: 10,
-      gender: "female",
-      neutered: false,
-      vaccination_status: true,
-      chip: true,
-      passport: false,
-      bio: "strong independent pet with sugary barks and many fluffs",
-      diet: "gluten-free kibble sprinkled with gold.",
-
-    }
-
-    const avocado = {
-      name: "avocado",
-      breed_id: 36,
-      user_id: 25,
-      age: 2,
-      gender: "male",
-      neutered: false,
-      vaccination_status: true,
-      chip: false,
-      passport: true,
-      bio: "from a seed to a wooffy fruit",
-      diet: "just NOT avocadoes",
-
-    }
-
-
-    pets = [bot, fluff, sugarpuff, avocado]
-
-    await queryInterface.bulkInsert("pets", pets, {})
+        diet: "only strawberries and champagne.",
+      },
+      {
+        name: "sugarpuff",
+        breed_id: 12,
+        user_id: 25,
+        age: 10,
+        gender: "female",
+        neutered: false,
+        vaccination_status: true,
+        chip: true,
+        passport: false,
+        bio: "strong independent pet with sugary barks and many fluffs",
+        diet: "gluten-free kibble sprinkled with gold.",
+      },
+      {
+        name: "avocado",
+        breed_id: 36,
+        user_id: 25,
+        age: 2,
+        gender: "male",
+        neutered: false,
+        vaccination_status: true,
+        chip: false,
+        passport: true,
+        bio: "from a seed to a wooffy fruit",
+        diet: "just NOT avocadoes",
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('pets', null, {});
-
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("pets", null);
+  },
 };
