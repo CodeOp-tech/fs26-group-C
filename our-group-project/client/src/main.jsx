e9efddcc6b07ced4dd5c853f785e84a2771387a8
Veronica Rojas <ar_veronica@yahoo.com>
2023-06-14 17:53:35 +0100

    remove code causing error

--- our-group-project/client/src/main.jsx
+++ our-group-project/client/src/main.jsx
@@ -5,10 +5,7 @@ import "./index.css";
 import {BrowserRouter} from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+
 
 const defaultTheme = createTheme({
   palette: {
