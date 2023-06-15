18fb2fc905951f536d1bcd9b376b8fe739a6ed20
loretta savvidou <loretta.sav.6@gmail.com>
2023-06-15 23:29:33 +0200

    edit

--- our-group-project/client/src/assets/pages/UserProfile.jsx
+++ our-group-project/client/src/assets/pages/UserProfile.jsx
@@ -6,16 +6,15 @@ import AuthContext from "../contexts/AuthContext";
 //import TextField from "@mui/material/TextField";
 import { IconButton, Button } from "@mui/material";
 import { NearMeOutlined } from "@mui/icons-material";
-import TextareaAutosize from '@mui/base/TextareaAutosize';
-
+import TextareaAutosize from "@mui/base/TextareaAutosize";
 
 export default function UserProfile() {
   const auth = useContext(AuthContext);
-  
+
   useEffect(() => {
     requestPrivateData();
   }, []);
-  
+
   const [data, setData] = useState({});
   const requestPrivateData = async () => {
     try {
@@ -24,10 +23,10 @@ export default function UserProfile() {
           authorization: "Bearer " + localStorage.getItem("token"),
         },
       });
-      console.log("data",data)
-      setData({data});
+      console.log("data", data);
+      setData({ data });
     } catch (error) {
-      console.log("error",error);
+      console.log("error", error);
     }
   };
 
@@ -52,27 +51,23 @@ export default function UserProfile() {
               <h6> Some catchy Phrase</h6>
             </div>
             <div className="row" style={{ paddingTop: "1vw" }}>
-              <IconButton style={{ width: "55%" , fontSize:"1.4vw"}} disabled>
+              <IconButton style={{ width: "55%", fontSize: "1.4vw" }} disabled>
                 <LocationOnIcon
                   color="secondary"
-                  style={{ display: "inline", margin:"0.5vw"}}
-                
+                  style={{ display: "inline", margin: "0.5vw" }}
                 />
                 {auth.location}
               </IconButton>
             </div>
-            <div className="row">
-              <label htmlFor="toadopt"> Give a home </label>
-              <input type="radio" id="toadopt" />
-              <label htmlFor="togiveforadoption"> Find a new home</label>
-              <input type="radio" id="togiveforadoption" />
-            
             </div>
-          </div>
         </div>
       </div>
-      <div className="row" style={{margin:"2vw 5.5vw"}}>
-        <TextareaAutosize placeholder="Let us know who you are!"  minRows={10} sx={{border: "0.5vw solid " }}></TextareaAutosize>
+      <div className="row" style={{ margin: "2vw 5.5vw" }}>
+        <TextareaAutosize
+          placeholder="Let us know who you are!"
+          minRows={10}
+          sx={{ border: "0.5vw solid " }}
+        ></TextareaAutosize>
       </div>
     </div>
   );
