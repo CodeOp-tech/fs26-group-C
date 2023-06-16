81002eb4bf091a9d43fc65576891a74ff9ff995a
Veronica Rojas <ar_veronica@yahoo.com>
2023-06-16 12:22:44 +0100

    checking colors

--- our-group-project/client/src/assets/pages/ContactUs.jsx
+++ our-group-project/client/src/assets/pages/ContactUs.jsx
@@ -1,84 +1,27 @@
-import {
-  Checkbox,
-  TextField,
-  FormControlLabel,
-  Avatar,
-  Button,
-  InputAdornment,
-  IconButton,
-  CssBaseline,
-  Grid,
-  Box,
-  Typography,
-  Container,
-} from "@mui/material/";
-import { Link } from "react-router-dom";
-import ContactSupportIcon from "@mui/icons-material/ContactSupport";
-import { TextareaAutosize } from "@mui/material/";
+import React from 'react'
+import { Container, Button, Box, Grid, TextField } from "@mui/material"
 
-function Copyright(props) {
+export default function ContactUs() {
   return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" to="/about">
-        our website name
-      </Link>
-      {new Date().getFullYear()}
-    </Typography>
-  );
-}
+    <Container component="main"
+    maxWidth="xs"
+    sx={{
+      mt: 10,
+    }}>
+      <Grid container spacing={2} >
+        <Grid item xs={12} sm={12}>
+        <TextField
+        placeholder='Send us a message'
+        fullWidth></TextField>
+        </Grid>
 
-function ContactUs() {
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <ContactSupportIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            What would you like to know?
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <Grid container spacing={2}></Grid>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="email"
-              name="email"
-            />
+      
 
-            <TextareaAutosize minRows={5} style={{ padding: "2vw 10vw", textAlign:"left" }} placeholder="Talk to us!">
-              
-            </TextareaAutosize>
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Submit
-            </Button>
-          </Box>
-        </Box>
-      </CssBaseline>
+      <Button variant='contained'>hello</Button>
+
+      </Grid>
+
     </Container>
-  );
+  )
 }
-
-export default ContactUs;
