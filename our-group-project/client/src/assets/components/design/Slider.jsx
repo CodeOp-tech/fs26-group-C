3202020822b662edd7b50cbf1ecf94965dcf38a2
Veronica Rojas <ar_veronica@yahoo.com>
2023-06-16 12:43:42 +0100

    fixed the colors

--- our-group-project/client/src/assets/components/design/Slider.jsx
+++ our-group-project/client/src/assets/components/design/Slider.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import Box from "@mui/material/Box";
-import Slider from "@mui/material/Slider";
+import { Slider } from "@mui/material";
 
 const marks = [
   
@@ -33,20 +34,25 @@ function valueLabelFormat(value) {
   return marks.findIndex((mark) => mark.value === value) + 1;
 }
 
+
+
+
 export default function DiscreteSliderValues() {
   return (
     <Box sx={{ width: "30%" }}>
-      <Slider
-        aria-label="Restricted values"
-        defaultValue={0}
-        valueLabelFormat={valueLabelFormat}
-        getAriaValueText={valuetext}
-        step={null}
-        valueLabelDisplay="auto"
-        marks={marks}
-        min={1}
-        max={5}
-      />
-    </Box>
-  );
+    <Slider
+    color="secondary"
+      aria-label="Restricted values"
+      defaultValue={0}
+      valueLabelFormat={valueLabelFormat}
+      getAriaValueText={valuetext}
+      step={null}
+      valueLabelDisplay="auto"
+      marks={marks}
+      min={1}
+      max={5}
+    />
+  </Box>
+
+  )
 }
