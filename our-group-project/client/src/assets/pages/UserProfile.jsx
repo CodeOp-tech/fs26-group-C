7ba1d559ff60746d4a5ee2cf38462f50b7149f7c
loretta savvidou <loretta.sav.6@gmail.com>
2023-06-16 10:30:49 +0200

    test

--- our-group-project/client/src/assets/pages/UserProfile.jsx
+++ our-group-project/client/src/assets/pages/UserProfile.jsx
@@ -12,11 +12,11 @@ import Slider from '../components/design/Slider'
 
 export default function UserProfile() {
   const auth = useContext(AuthContext);
-
+  
   useEffect(() => {
     requestPrivateData();
   }, []);
-
+  
   const [data, setData] = useState({});
   const requestPrivateData = async () => {
     try {
@@ -25,10 +25,10 @@ export default function UserProfile() {
           authorization: "Bearer " + localStorage.getItem("token"),
         },
       });
-      console.log("data", data);
-      setData({ data });
+      console.log("data",data)
+      setData({data});
     } catch (error) {
-      console.log("error", error);
+      console.log("error",error);
     }
   };
 
@@ -52,22 +52,22 @@ export default function UserProfile() {
             <div className="row" style={{ paddingTop: "2vw" }}>
               <h6> Some catchy Phrase</h6>
             </div>
-            </div>
             <div className="row" style={{ paddingTop: "1vw" }}>
-              <IconButton style={{ width: "55%", fontSize: "1.4vw" }} disabled>
+              <IconButton style={{ width: "55%" , fontSize:"1.4vw"}} disabled>
                 <LocationOnIcon
                   color="secondary"
-                  style={{ display: "inline", margin: "0.5vw" }}
+                  style={{ display: "inline", margin:"0.5vw"}}
+                
                 />
                 {auth.location}
               </IconButton>
             </div>
-            <div className="row">
-            </div>
+          </div>
+        </div>
       </div>
       <div className="row" style={{margin:"2vw 5.5vw", display:"flex"}}>
         <TextareaAutosize placeholder="Let us know who you are!"  minRows={10} sx={{border: "0.5vw solid " }}></TextareaAutosize>
-
+      
         <div className="col" style={{marginTop:"2vw"}}>
         {
           auth.adopter ? <div><TextareaAutosize placeholder="Why do you want to adopt?"  minRows={5} sx={{border: "0.5vw solid " }}></TextareaAutosize>
@@ -94,6 +94,7 @@ export default function UserProfile() {
 
         </div>
 
+        
         <div className="col" style={{marginTop:"2vw"}}>
         {
           !auth.adopter ? <div><TextareaAutosize placeholder="Why do you want to adopt?"  minRows={5} sx={{border: "0.5vw solid " }}></TextareaAutosize>
@@ -112,10 +113,30 @@ export default function UserProfile() {
             <label> Large Space available</label>
             <Slider />
           </div>
-          
+          <div >
+            <label> Something else</label>
+            <Slider  />
+          </div>
+          </div>
+          <div className="col" style={{marginTop:"1vw"}}>
+          <div >
+            <label> More things</label>
+            <Slider  />
+          </div>
+          <div >
+            <label> We love slidin</label>
+            <Slider />
+          </div>
+          <div >
+            <label> *Drake said it best* </label>
+            <Slider  />
+          </div>
+        </div>
         </div>
 
+        
       </div>
+      
     </div>
-  )
-};
+  );
+}
