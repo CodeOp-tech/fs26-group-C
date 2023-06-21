c406a8d2ab123cbdbfeb680dbe21efd97f4da77a
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-06-21 11:27:50 +0200

    quiz- test

--- our-group-project/migrations/20230620144436-create-weight-data.js
+++ our-group-project/migrations/20230620144436-create-weight-data.js
@@ -1,28 +1,51 @@
-'use strict';
+
+const models = require("../models");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('weight_data', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      question: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+
+    // class Weight extends Breed {
+    //   static associate(models) {
+    //     // define association here
+    //     Weight.hasOne(models.Breed,{foreignKey:"breed_id"})
+    //   }
+
+    // }
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    // create the new columns
+    await queryInterface.addColumn("breeds", { minWeight: Sequelize.INTEGER });
+    await queryInterface.addColumn("breeds", { maxWeight: Sequelize.INTEGER });
+
+    // populate the new columns with the data from the old weight column
+    const breeds = await models.Breed.findAll();
+    // generate a new array of breeds, in which the minWeidht and maxWeight are set
+    // foreach each breed in breeds
+      breeds.forEach( async (breed) => {
+        const minweight = Number(breed.weight.split(" - ")[0])
+        const maxweight = Number(breed.weight.split(" - ")[1])
+        // "set" the minWeight and maxWeight
+        breed.set({
+          minWeight: minweight,
+          maxWeight: maxweight
+        });
+    
+        await breed.save()
+      })
   },
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('weight_data');
-  }
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
 };
